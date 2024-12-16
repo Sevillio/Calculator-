e4c4089c0764fddc6a9bda0c4bd77f569eec231a
Sevillio <113693175+Sevillio@users.noreply.github.com>
2024-12-17 00:23:18 +0100

    first commit

---
+++ Calculator/Program.cs
@@ -0,0 +1,70 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System.Security.Cryptography;
+
+Console.WriteLine("What is your name");
+string Userinput = Console.ReadLine();
+Console.WriteLine($"Hello, {Userinput} !");
+
+int a  = 0;
+int b = 0;
+int c = 0;
+Console.WriteLine();
+Console.ReadKey();
+
+//Declaration variables 
+var num1 = 0; var num2 = 0;
+
+//Display the Title in the C# Calculator 
+Console.WriteLine("Console Calculator in C#\r");
+Console.WriteLine(" ------------------------\n ");
+
+Console.WriteLine("Fill in the first number and then press Enter");
+num1 = Convert.ToInt32(Console.ReadLine());
+Console.ReadKey();
+
+
+Console.WriteLine("Fill in the second number and then press Enter");
+num2 = Convert.ToInt32(Console.ReadLine());
+Console.ReadKey();
+
+// Ask the user to choose an option.
+Console.WriteLine("Choose an option from the following list:");
+Console.WriteLine("\ta - Add");
+Console.WriteLine("\ts - Subtract");
+Console.WriteLine("\tm - Multiply");
+Console.WriteLine("\td - Divide");
+Console.WriteLine("\tp - Percentage");
+Console.WriteLine("\tt - to the power");
+Console.Write("Your option? ");
+
+// Use a switch statement to do the math.
+switch (Console.ReadLine())
+{
+    case "a":
+        var resulta = (num1 + num2);
+        Console.WriteLine($"Your result: {num1} + {num2} = " +resulta);
+        break;
+    case "s":
+        Console.WriteLine($"Your result: {num1} - {num2} = " + (num1 - num2));
+        break;
+    case "m":
+        Console.WriteLine($"Your result: {num1} * {num2} = " + (num1 * num2));
+        break;
+    case "d":
+        double resultd = ((double)num1 /(double)num2);
+        Console.WriteLine($"Your result: {num1} / {num2} = " +resultd);
+        break;
+    case "p":
+        double resultp = ((double) num1 / (double) num2)*100;
+        Console.WriteLine($"Your result: " + resultp + "%");
+        break;
+    case "t":
+        int resultt=(int)num1*(int)num1;
+        Console.WriteLine($"your result:{resultt}");
+        break;
+}
+// Wait for the user to respond before closing.
+Console.Write("Press any key to close the Calculator console app...");
+Console.ReadKey();
---
+++ Form1.cs
@@ -0,0 +1,99 @@
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            button1.Text = "1";
+            button2.Text = "2";
+            button3.Text = "3";
+            button4.Text = "4";
+            button5.Text = "5";
+            button6.Text = "6";
+            button7.Text = "7";
+            button8.Text = "8";
+            button9.Text = "9";
+            button16.Text = "0";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Text = button1.Text()
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
